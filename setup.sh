11d606ad962a5c504e29815ffa587c2f65447a72
James Tooze <toozej@gmail.com>
2022-06-30 15:05:06 -0700

    migrate dotfiles to GHA

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd  )"
 
 # symlink files from within ./common into their correct places within ~/
 echo -e "installing common dotfiles\n"
-for item in `ls -a $DIR/common/`
+for item in $(ls -a "${DIR}"/common/)
 do
-    echo $item
-    ln -snf $DIR/common/$item ~
+    echo "${item}"
+    ln -snf "${DIR}"/common/"${item}" ~
 done
 echo -e "setting correct permissions on ~/.gnupg\n"
 chmod 0700 ~/.gnupg
@@ -16,20 +16,20 @@ git config --global init.templateDir ~/.git-template
 
 echo -e "installing local binaries\n"
 mkdir ~/bin
-for item in `ls -a $DIR/bin/`
+for item in $(ls -a "${DIR}"/bin/)
 do
-    echo $item
-    ln -snf $DIR/bin/$item ~/bin/
+    echo "${item}"
+    ln -snf "${DIR}"/bin/"${item}" ~/bin/
 done
 curl https://raw.githubusercontent.com/toozej/git-peak-extended/main/git-peak-extended > ~/bin/git-peak-extended && chmod ug+x ~/bin/git-peak-extended
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
 
 echo -e "installing local Templates\n"
 mkdir ~/Templates
-for item in `ls -a $DIR/Templates/`
+for item in $(ls -a "${DIR}"/Templates/)
 do
-    echo $item
-    ln -snf $DIR/Templates/$item ~/Templates/
+    echo "${item}"
+    ln -snf "${DIR}"/Templates/"${item}" ~/Templates/
 done
 
 # clone and install vimfiles
@@ -43,7 +43,7 @@ vim +PlugInstall +qall
 echo -e "determining OS and distro, then installing related dotfiles\n"
 if [ "$(uname)" == "Darwin" ]; then
     # ensure .oh-my-zsh is installed
-    if [[ ! -d "~/.oh-my-zsh" ]]; then
+    if [[ ! -d "${HOME}/.oh-my-zsh" ]]; then
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
         # install powerline patched fonts for use in iTerm
@@ -52,42 +52,42 @@ if [ "$(uname)" == "Darwin" ]; then
     fi
 
     # symlink oh-my-zsh theme in place
-    ln -sf $DIR/mac/.oh-my-zsh/custom/themes/agnoster.zsh-theme ~/.oh-my-zsh/custom/themes/agnoster.zsh-theme
+    ln -sf "${DIR}"/mac/.oh-my-zsh/custom/themes/agnoster.zsh-theme ~/.oh-my-zsh/custom/themes/agnoster.zsh-theme
     # remove default .zshrc
     rm -f ~/.zshrc
 
     # copy karabiner config in place since the symlink gets overwritten when opening Karabiner-Elements 
-    cp $DIR/mac/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
+    cp "${DIR}"/mac/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
 
     # tell iTerm to use my default dynamic profile
-    ln -sf $DIR/mac/iterm_default_profile.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/iterm_default_profile.json
+    ln -sf "${DIR}"/mac/iterm_default_profile.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/iterm_default_profile.json
 
     # symlink files from within ./mac/ to their correct location
     echo -e "installing dotfiles for MacOS"
-    for item in `ls -a $DIR/mac/`
+    for item in $(ls -a "${DIR}"/mac/)
     do
-        echo $item
-        ln -snf $DIR/mac/$item ~
+        echo "${item}"
+        ln -snf "${DIR}"/mac/"${item}" ~
     done
 
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+elif [ "$(uname -s)" == "Linux" ]; then
     # if running i3-wm
-    if [ "$(which i3)" == "/usr/bin/i3" ]; then
+    if [ "$(command -v i3)" == "/usr/bin/i3" ]; then
         echo -e "installing dotfiles for i3-wm"
-        for item in `ls -a $DIR/gui/i3`
+        for item in $(ls -a "${DIR}"/gui/i3)
         do
-            echo $item
-            ln -snf $DIR/gui/i3/$item ~
+            echo "${item}"
+            ln -snf "${DIR}"/gui/i3/"${item}" ~
         done
     fi
 
-    if [ "$(which gvim)" == "/usr/bin/vim.gtk3" ] || [ "$(which gvim)" == "/usr/bin/vim.gnome" ]; then
+    if [ "$(command -v gvim)" == "/usr/bin/vim.gtk3" ] || [ "$(command -v gvim)" == "/usr/bin/vim.gnome" ]; then
         echo -e "installing dotfiles for gvim"
-        ln -sf $DIR/gui/.gvimrc ~/.gvimrc
+        ln -sf "${DIR}"/gui/.gvimrc ~/.gvimrc
     fi
 fi
 
 
 # reminders
 echo -e "\nDon't forget to set secret things in the following files:"
-echo -e "~/.ssh/config\n"
+echo -e "${HOME}/.ssh/config\n"
